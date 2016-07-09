3fd5ed3197452c2a662acc7edf6005209c98da05
Chris Vandenberg <chrisvan0848@gmail.com>
2016-07-09 08:43:29 -0700

    modify home page

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 
 export default class extends Component {
-  constructor() {
-    super()
-  }
-
   render() {
     var imageList = this.props.images.map(image => {
       return (
         <div id="img-container">
           <img src={image.src} />
-          <div className="img-overlay">Image</div>
+          <div className="img-overlay"></div>
         </div>
       )
     })
